d7c5f7980b0bc1b57d89b8251827e82f4b13ef3b
dooinn <kimdooinn1@gmail.com>
2026-09-12 16:16:12 +0200

    Update user_functions

--- unit_testing_best_practice/src/user_functions.py
+++ unit_testing_best_practice/src/user_functions.py
@@ -7,3 +7,27 @@ def get_email_from_input():
     else:
         return email
 
+def get_username_from_input():
+    """ Not empty, no spaces """
+    username = input("Tell me your username: ")
+
+    if (username == "" or " " in username):
+        print('Username is not valid.')
+    else:
+        return username
+
+
+def get_password_from_input():
+    """ At least 8 chars, 1 letter, 1 number, 1 special char """
+    import re
+    password = input("Tell me your password: ")
+
+    has_letter = re.search(r"[a-zA-Z]", password)
+    has_number = re.search(r"[0-9]", password)
+    has_special = re.search(r"[^a-zA-Z0-9]", password)
+
+    if (len(password) < 8 or not has_letter or not has_number or not has_special):
+        print('Password is not valid.')
+    else:
+        return password
+
